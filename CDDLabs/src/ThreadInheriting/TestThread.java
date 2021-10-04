c3d5ece8421715c9d84061e7238ee3b0cbf45b73
Michelle Bolger <michellebolgerlaois@gmail.com>
2021-10-04 11:15:42 +0100

    CDD Lab 1 Added JavaDoc comments to the code

---
+++ CDDLabs/src/ThreadInheriting/TestThread.java
@@ -0,0 +1,28 @@
+package ThreadInheriting;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import ThreadInheriting.ThreadDemo;
+
+/**
+ *
+ * @author KEHOEJ
+ * @author Michelle Bolger
+ *
+ * Create two threads and run them.
+ */
+public class TestThread {
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        ThreadDemo T1 = new ThreadDemo( "Thread-1");
+        T1.start();
+        ThreadDemo T2 = new ThreadDemo( "Thread-2");
+        T2.start();
+    }
+}
---
+++ CDDLabs/src/ThreadingRunnable/TestThread.java
@@ -0,0 +1,25 @@
+package ThreadingRunnable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author KEHOEJ
+ * @author Michelle Bolger
+ */
+public class TestThread {
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
+        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+        R1.start();
+        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+        R2.start();
+    }
+
+}
